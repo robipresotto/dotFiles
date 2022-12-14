640409e89ec4ee503a576df3164604b674ae4906
Robinson Presotto <robipresotto@gmail.com>
2022-12-14 16:33:14 +0100

    Cleanup

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -2,3 +2,7 @@
 function colormap() {
   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
+
+function healthCheck() {
+  say $1
+}
--- .zshenv
+++
@@ -1,7 +0,0 @@
-W=`whoami`
-
-# Rust/Cargo
-. "$HOME/.cargo/env"
-
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-export PATH="/Users/$W/.rd/bin:$PATH"
