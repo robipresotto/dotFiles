da960301533658ecb80c8111d17b408e0db0b6ab
Robinson Presotto <robipresotto@gmail.com>
2022-12-14 18:20:01 +0100

    Add health check

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -3,6 +3,8 @@ function colormap() {
   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
 
-function healthCheck() {
-  say $1
+# HealthCheck
+# ex. hc www.apple.com
+function hc() {
+  while true; do ping -c1 $1 && sleep 1 > /dev/null && echo works && say works; break; done
 }
