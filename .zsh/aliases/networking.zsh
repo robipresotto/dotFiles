157d25e44424335ee85d58bcf1a4d9b9a8257e22
Robinson Presotto <robipresotto@gmail.com>
2022-12-15 21:34:30 +0100

    Fix networking functions

--- .zsh/aliases/networking.zsh
+++ .zsh/aliases/networking.zsh
@@ -1,4 +1,3 @@
 # Networking
-alias listen="sudo ngrep -q -d 'en0' -W byline '' 'host $argv'"
 alias ipe="curl ipecho.net/plain; echo"
 alias ipi="ipconfig getifaddr en0"
--- .zsh/functions/networking.zsh
+++ .zsh/functions/networking.zsh
@@ -1,9 +1,15 @@
 # Networking
 function ipinfo() {
+  # ipinfo x.x.x.x
   curl 'https://ipinfo.io/'$1'?token='$IPINFO_TOKEN''
 }
 
+function listen() {
+  # listen localhost
+  sudo ngrep -q -d 'en0' -W byline '' 'host '$1''
+}
+
 function hc() {
-  # ex. hc www.apple.com
+  # hc www.apple.com
   while true; do ping -c1 $1 && sleep 1 > /dev/null && echo works && say works; break; done
 }
