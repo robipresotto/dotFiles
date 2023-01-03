e1c831c37b44d3a2fce7d06a975d92f665073699
Robinson Presotto <robipresotto@gmail.com>
2023-01-03 21:09:51 +0100

    K8 function

--- .zsh/functions/kubernetes.zsh
+++ .zsh/functions/kubernetes.zsh
@@ -3,3 +3,8 @@
 function k8gpip() {
   kubectl describe pods $1 -n $2 | grep -m1 IP | awk '{print $2}'
 }
+
+function k8cc() {
+  # k8cc hello */1 * * * * echo "hello"
+  kubectl create cronjob $1 --image=busybox:1.28   --schedule="$2" -- $3
+}
