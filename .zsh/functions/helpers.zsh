f6dbdc7db6dcd4da275baa703fb1c6cd9a6f27b1
Robinson Presotto <robipresotto@gmail.com>
2023-01-12 17:01:18 +0100

    Fix comments for files

--- .zsh/functions/helpers.zsh
+++ .zsh/functions/helpers.zsh
@@ -10,7 +10,7 @@ function listAliases() {
   local files=("${(@f)$(ls -p ~/.zsh/aliases)}")
   for file in $files
   do
-    echo "\n\033[36m# ${file}\n\033[37m"
+    echo "\n\033[36m◉ [aliases] ${file}\n\033[37m"
     cat ~/.zsh/aliases/$file | grep 'alias' | sed 's/alias/>/g' | sort
   done
 }
@@ -19,14 +19,14 @@ function listFunctions() {
   local files=("${(@f)$(ls -p ~/.zsh/functions)}")
   for file in $files
   do
-    echo "\n\033[36m# ${file}\n\033[37m"
+    echo "\n\033[36m◉ [functions] ${file}\n\033[37m"
     cat ~/.zsh/functions/$file | sed -n '/function/,/}/p'
   done
 }
 
 function help() {
-  listAliases | sed -n "/$1/,/#/p" | sed '$d'
-  listFunctions | sed -n "/$1/,/#/p" | sed '$d'
+  listAliases | sed -n "/$1/,/◉/p" | sed '$d'
+  listFunctions | sed -n "/$1/,/◉/p" | sed '$d'
 }
 
 function defaultBrowser() {
