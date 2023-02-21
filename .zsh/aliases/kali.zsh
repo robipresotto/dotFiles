b5f7774d051b1b865935bbf530784b29689d61bd
Robinson Presotto <robipresotto@gmail.com>
2023-02-21 18:43:35 +0100

    Add kali aliases

---
+++ .zsh/aliases/kali.zsh
@@ -0,0 +1,8 @@
+alias nmaph="nmap -v -sS -A -T4"
+alias nmapha="nmap -A -T4"
+alias nmaptcph="nmap -v -sS -p–A -T4"
+alias nmapah="nmap -v -sU -sS -p- -A -T4"
+alias nmapl="nmap localhost"
+alias nmap="nmap -sV --script=vulscan/vulscan.nse"
+
+alias wfuzz="wfuzz -w"
