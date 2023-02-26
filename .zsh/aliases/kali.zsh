6c89ad9ee00778aa92a76237267c0224b7cecc86
Robinson Presotto <robipresotto@gmail.com>
2023-02-26 12:10:51 +0100

    Cleanup networking aliases

--- .zsh/aliases/kali.zsh
+++
@@ -1,8 +0,0 @@
-alias nmaph="nmap -v -sS -A -T4"
-alias nmapha="nmap -A -T4"
-alias nmaptcph="nmap -v -sS -p–A -T4"
-alias nmapah="nmap -v -sU -sS -p- -A -T4"
-alias nmapl="nmap localhost"
-alias nmapnse="nmap -sV --script=vulscan/vulscan.nse"
-
-alias wfuzz="wfuzz -w"
--- .zsh/aliases/networking.zsh
+++ .zsh/aliases/networking.zsh
@@ -1,2 +1,14 @@
 alias ipe="curl ipecho.net/plain; echo"
 alias ipi="ipconfig getifaddr en0"
+
+alias nstatrt="netstat -rWn"
+alias routen="route -n"
+
+alias nmaph="nmap -v -sS -A -T4"
+alias nmapha="nmap -A -T4"
+alias nmaptcph="nmap -v -sS -p–A -T4"
+alias nmapah="nmap -v -sU -sS -p- -A -T4"
+alias nmapl="nmap localhost"
+alias nmapnse="nmap -sV --script=vulscan/vulscan.nse"
+
+alias wfuzz="wfuzz -w"
